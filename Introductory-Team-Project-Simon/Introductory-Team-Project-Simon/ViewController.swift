94338a7f6d0060469abc8dee9c74ee52a48de2eb
Ryan Dawkins <ryan@ryanddawkins.com>
2015-09-04 19:25:11 -0500

    Revert ".gitignore is now in effect"
    This reverts commit 104b151dd9aad2b16e77e6894e528a6f1dc0753a.

---
+++ Introductory-Team-Project-Simon/Introductory-Team-Project-Simon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Introductory-Team-Project-Simon
+//
+//  Created by Team Blue on 9/3/15.
+//  Copyright © 2015 Team Blue Mobile App 2015. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
