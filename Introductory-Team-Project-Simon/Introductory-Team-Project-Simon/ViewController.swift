7fb6915d01e2b579845a80a82e8f5604099db599
Jordan Fest <jordan.fest1@gmail.com>
2015-09-12 19:03:48 -0500

    UI Implementation into previous master iteration

--- Introductory-Team-Project-Simon/Introductory-Team-Project-Simon/ViewController.swift
+++ Introductory-Team-Project-Simon/Introductory-Team-Project-Simon/ViewController.swift
@@ -21,5 +21,49 @@ class ViewController: UIViewController {
         let pattern = game.getPattern()
         print(pattern.description)
     }
+    
+    @IBAction func NewGame_Tap(sender: UIButton) {
+        print("New Game");
+    }
+    
+    //GREEN Button
+    @IBAction func Green_Button_Tap(sender: UIButton) {
+        print("0 - Green Button")
+    }
+    
+    @IBAction func Green_Button_Release(sender: UIButton) {
+        print("-Green Button")
+    }
+    
+    //RED Button
+    @IBAction func Red_Button_Tap(sender: UIButton) {
+        print("1 - Red Button")
+    }
+    
+    @IBAction func Red_Button_Release(sender: UIButton) {
+        print("-Red Button")
+    }
+    
+    //YELLOW Button
+    @IBAction func Yellow_Button_Tap(sender: UIButton) {
+        print("2 - Yellow Button")
+    }
+    
+    @IBAction func Yellow_Button_Release(sender: UIButton) {
+        print("-Yellow Button")
+    }
+    
+    //BLUE Button
+    @IBAction func Blue_Button_Tap(sender: UIButton) {
+        print("4 - Blue Button")
+    }
+    
+    @IBAction func Blue_Button_Release(sender: UIButton) {
+        print("-Blue Button")
+    }
+    
+    @IBOutlet weak var highscore: UITextField!
+    
+    @IBOutlet weak var current_score: UITextField!
 }
 
